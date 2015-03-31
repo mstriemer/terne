1215eec20ad490a56432aa0733d8fdf4134a0550
Mark Striemer <mstriemer@mozilla.com>
2015-03-30 20:29:43 -0500

    ...

--- sample/alloy-bar/alloy-bar.es6.js
+++ sample/alloy-bar/alloy-bar.es6.js
@@ -4,7 +4,8 @@ class AlloyBar extends HTMLElement {
         console.log(this.tagName, this.alloy);
         this.originalContent = this.innerHTML;
         var template = this.alloy.content.cloneNode(true);
-        template.querySelector('.alloy-bar-wrapper').appendChild(this.querySelector('h1'));
+        var h1 = this.querySelector('h1');
+        template.querySelector('.alloy-bar-wrapper').appendChild(h1);
         this.appendChild(template);
     }
 }
